beff5ba1939c8154816a4e9fcf4d68028ba91fbb
Juan <jestebanl88m@gmail.com>
2024-06-07 14:01:54 +0200

    gameManager

--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -1,22 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 
 public class GameManager : MonoBehaviour
 {
-
+    
     public void Menu()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(0);
     }
     
     public void Play()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
     }
 
     public void Defeat()
