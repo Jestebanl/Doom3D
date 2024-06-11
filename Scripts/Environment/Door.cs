1d6a67a7da8fe5c98c609ea6f6616ce3c195b697
Marcos Fernández Núñez <fernandemarcos11@gmail.com>
2024-06-11 12:42:43 +0200

    comentarios door.cs

--- Scripts/Environment/Door.cs
+++ Scripts/Environment/Door.cs
@@ -5,11 +5,11 @@ public class Door : MonoBehaviour
 {
 
 
-    public Animator doorAnim;
-
+    public Animator doorAnim;//animacion cuando la puerta se abre
+    //como las puertas se abren solo si tienes la llave de acceso correcta, hacemos un booleano para comprobarlo
     public bool requireskey;
-    public bool reqRed, reqGreen, reqBlue;
-    public bool isOpen;
+    public bool reqRed, reqGreen, reqBlue;//hay tres puertas diferentes con tres llaves de acceso diferentes
+    public bool isOpen;//booleano ppara abrir la puerta
 
 
 
@@ -17,27 +17,36 @@ public class Door : MonoBehaviour
 
     private void Start()
     {
+        //al iniciar el programa, la puerta obviamente esta cerrada
         isOpen = false;
     }
 
     private void Update()
     {
+        //si la puerta esta abierta:
         if (isOpen)
         {
+            //se destrulle el boxcollider que envolvia a la puerta, para que el player pueda pasar a traves de ella
             Destroy(gameObject.GetComponent<BoxCollider>());
         }
     }
-
+    //funcion que sirve para detectar al player si entra dentro del collider de la puerta y abrir la puerta si tiene la llave
     void OnTriggerEnter(Collider other)
     {
+        //si lo que entra dentro del collider de la puerta tiene la tag de "player"
         if (other.CompareTag("Player"))
         {
-
+            //si la puerta requiere una llave de acceso:
             if (requireskey)
             {
+                // como hay tres llaves diferentes, tenemos que comprobar cual es la llave que tiene
+                // y abrir la puerta si es la correcta
                 if (reqRed && other.GetComponent<PlayerInventory>().hasRed)
                 {
+                    //si es la puerta roja y tiene la llave de acceso roja se inicia la animacion "openDoor"
                     doorAnim.SetTrigger("OpenDoor");
+                    
+                    //el boolean de esta abierta se vuelve true para eliminar el boxCollider para que el player pueda pasar
                     isOpen = true;
                     
                 }
@@ -57,6 +66,7 @@ public class Door : MonoBehaviour
                 }
 
             }
+            //Como hay puertas que no requieren llaves (las iniciales) las abrimos directamente
             else
             {
                 doorAnim.SetTrigger("OpenDoor");
