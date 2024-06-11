d1295f75a1ace6ebf5ee5bb40a99028721a5aa0b
Fsolanaa <fsolanaaranega@gmail.com>
2024-06-11 10:03:08 +0200

    Comentarios de Enemy.AI

--- Scripts/Enemy/EnemyAI.cs
+++ Scripts/Enemy/EnemyAI.cs
@@ -19,14 +19,17 @@ public class EnemyAI : MonoBehaviour
 
     private void Update()
     {
+        //Comprobamos si el enemigo esta muerto (Si el enemigo esta muerto no hace nada )
         if (!enemy.estaMuerto)
         {
+            //Comprobamos si el enemigo está en estado de agresión , y si es así, el enemigo mersigue al personaje 
             if (enemyAwareness.isAggro)
             {
                 enemyNavMeshAgent.SetDestination(playersTransform.position);
             }
             else
             {
+                //Con esto conseguimos que si el enemigo no esta en estado de agresión , se quede quieto en su posición asignada 
                 enemyNavMeshAgent.SetDestination(transform.position);
             }
         }
